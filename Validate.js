8f65e0caf06c13e1706d2aa45c94cbdb9449b674
Sara Giurca <sara_giurca_gemini_solutions@whirlpool.com>
2021-10-12 15:39:21 +0300

    Add the schema validation

---
+++ Validate.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let validator = require('tiny-json-validator');
+
+const data = require("./sample.json");
+
+let book_schema = {
+    type: 'object',
+    required: true,
+    properties: {
+        title: {
+            type: 'string',
+            required: true
+        },
+        author: {
+            type: 'object',
+            required: true,
+            properties: {
+                name: {
+                    type: 'string',
+                    required: true
+                },
+                age: {
+                    type: 'integer',
+                    required: true
+                },
+                city: {
+                    type: 'string'
+                }
+            }
+        },
+        related_titles: {
+            type: 'array',
+            required: true,
+            items: {
+                type: 'string'
+            }
+        }
+    }
+};
+
+let res = validator(book_schema, data);
+
+console.log(res.isValid)
+// false
+
+console.log(res.errors)
+// {author.age: "is required", related_titles.0: 'type must be string', related_titles.1: 'type must be string'}
